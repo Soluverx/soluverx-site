cdd8f8c0f035eabac59d61012f7fa8d99b3ff4b0
Eric Rodrigues Campos <ericzezim@gmail.com>
2026-09-12 12:15:31 -0300

    reverte fonte local por desempenho

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import '@fontsource/space-grotesk/400.css'
-import '@fontsource/space-grotesk/500.css'
-import '@fontsource/space-grotesk/600.css'
-import '@fontsource/space-grotesk/700.css'
-
 import App from './App'
 
 import './styles/reset.css'
